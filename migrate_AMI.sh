017cb0f86bb81b2caaa76a27df620592f8f9173b
Dave Bell <belldavidr@users.noreply.github.com>
2015-07-31 17:53:52 -0400

    Update migrate_AMI.sh

--- migrate_AMI.sh
+++ migrate_AMI.sh
@@ -28,7 +28,7 @@ OLD_INSTANCE=`aws ec2 describe-instances --filters "Name=instance-state-code,Val
 echo "Starting Instance..."
 INSTANCE_DETAILS=`aws ec2 run-instances --image-id $AMI_ID --key-name $KEY_ID --security-groups $SEC_ID --instance-type $SIZE --user-data file://./$BOOTSTRAP_SCRIPT --output text | grep INSTANCES`
 
-INSTANCE_ID=`echo $INSTANCE_DETAILS | awk '{print $7}'`
+INSTANCE_ID=`echo $INSTANCE_DETAILS | awk '{print $8}'`
 
 # wait for new instance to be started
 STATUS=`aws ec2 describe-instance-status --instance-ids $INSTANCE_ID --output text | grep INSTANCESTATUS | grep -v INSTANCESTATUSES | awk '{print $2;exit;}'`
